7489bd3e0ffd67ec3c081770ba5aa3dd44833db3
vasim.akram <vasim.akram@inventum.net>
2017-12-04 15:47:11 +0530

    template changes in hero and crisis-center

--- src/app/crisis-center/crisis-list.component.ts
+++ src/app/crisis-center/crisis-list.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core'
 @Component({
-    template: '<h2>Crisis center list here...</h2>'
+    template: `<h2>CRISIS CENTER</h2>
+    <p>Get your crisis-center here</p>
+    <button routerLink="/sidekicks">Go to sidekicks</button>`
   })
 export class CrisisListComponent {
 
--- src/app/heros/heroes-list.component.ts
+++ src/app/heros/heroes-list.component.ts
@@ -1,6 +1,8 @@
 import {Component ,OnInit} from '@angular/core'
 @Component({
-    template: '<h2>Hero list here...</h2>'
+    template: `<h2>HEROES</h2>
+    <p>Get your heroes here</p>
+    <button routerLink="/sidekicks">Go to sidekicks</button>`
   })
 export class HeroListComponent  {
 
